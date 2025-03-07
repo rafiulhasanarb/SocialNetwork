00f60fcad289fb83b165c8db14bc898fa7a8ba9b
rafiulhasanarb <rafiulhasanarb@gmail.com>
2025-03-07 14:08:02 +0600

    Initial Commit

---
+++ SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by Rafiul Hasan on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
